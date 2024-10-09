28d964d2088285cd93d8b964ac8653fbd1619d8b
elsayed136 <elsayed.a.eltaweel@gmail.com>
2024-10-09 15:21:50 +0300

    refactor: widget and project folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:quizizz_app/screens/home_page.dart';
+import 'package:quizizz_app/presentation/pages/home_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
