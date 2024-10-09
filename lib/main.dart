a570b72064022ae918c37368122c2818f3511bce
Abdullah Mohamed <abdullahmohamed137@gmail.com>
2024-10-09 04:33:18 +0300

    create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quizizz_app/screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quizizz App',
+      home: HomePage(),
     );
   }
 }
